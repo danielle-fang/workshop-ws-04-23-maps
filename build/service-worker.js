77abcc31d5b0b40e2a3c39c98b8da2a6a2b9039b
danielle-fang <danielle.fang.20@dartmouth.edu>
2019-04-23 22:12:46 -0400

    workshop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.0621ebe40bdfdd2455f0dcb043206962.js"
 );
 
 workbox.clientsClaim();
